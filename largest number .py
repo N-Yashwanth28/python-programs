f139587684a50f1c1f034bc44eda01dd302b9204
N-Yashwanth28 <netapalliyashwanth2022@gmail.com>
2026-05-24 10:31:10 +0530

    Create largest number .py

---
+++ largest number .py
@@ -0,0 +1,13 @@
+n1=int(input("Enter num1:"))
+n2=int(input("Enter num2:"))
+n3=int(input("Enter num3:"))
+
+if n1>n2 and n1>n3:
+  print("largest number is 1")
+
+elif n2>n1 and n2>n3:
+  print("largest number is 2")
+
+else:
+  print("largest number is 3")
+   
